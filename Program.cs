d4f69efefa8f108055c6c9328653e2a4d113a53a
alirezaZzzz <alirezabighami79@gmail.com>
2022-07-06 11:55:09 +0430

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using static System.Console;
+using System;
+
+int max = 100;
+for (int i = 1; i <= max; i++)
+{
+    if (i % 3 == 0 && i % 5 != 0)
+    {
+        Write("fizz");
+    } else if (i % 3 != 0 && i % 5 == 0)
+    {
+        Write("buzz");
+    } else if (i % 3 == 0 && i % 5 == 0)
+    {
+        Write("fizzbuzz");
+    } else
+    {
+        Write(i.ToString());
+    }
+    if (i < max)
+    {
+        Write(", ");
+    } else
+    {
+        Write(".");
+    }
+}
